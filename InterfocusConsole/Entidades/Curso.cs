ded01cf8bfcb2df732eef3aeac059e5fdb0e0d61
unknown <x10rodrigo@gmail.com>
2024-05-18 09:26:07 -0300

    arquivos do prof

--- InterfocusConsole/Entidades/Curso.cs
+++ InterfocusConsole/Entidades/Curso.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace  InterfocusConsole.Entidades
+﻿namespace InterfocusConsole.Entidades
 {
     public class Curso{
         public virtual int Id {get; set;}
